b8e6b27b8f8b4d5151d60018a775fc4d5566855d
John Komninos <johnkomninos@johns-air.myfiosgateway.com>
2022-06-30 14:22:51 -0400

    delete working

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,64 @@
 <script setup lang="ts">
-import {ref} from 'vue'
-import {onMounted} from 'vue'
-import axios from 'axios'
+  import {ref} from 'vue'
+  import {onMounted} from 'vue'
+  import axios from 'axios'
 
 //importing components
-import GET from './components/get.vue'
-import POST from './components/post.vue'
+  import GET from './components/get.vue'
+  import POST from './components/post.vue'
+  import DELETE from './components/delete.vue'
 
 //variables
-let foods = ref([])
+  let foods = ref([])
 
-let postNewItem = ref(
-    {
-      name:"",
-      category:"",
-      aisle:""
-    }
-  )
+  let postNewItem = ref(
+      {
+        name:"",
+        category:"",
+        aisle:""
+      }
+    )
 
 //methods
-onMounted(()=>{
+  onMounted(()=>{
     axios.get('https://grocerylists-backend.herokuapp.com/api/items').then(response => foods.value = response.data)
   })
 
   const handleCreate = () =>{
-      axios.post('https://grocerylists-backend.herokuapp.com/api/items', postNewItem.value)
-      .then((response)=>{
-        foods.value = [...foods.value, response.data]
-        postNewItem.value = ref(
-          {
-            name:"",
-            category:"",
-            aisle:""
-          }
-        )
-      })
-    }
+    axios.post('https://grocerylists-backend.herokuapp.com/api/items', postNewItem.value)
+    .then((response)=>{
+      foods.value = [...foods.value, response.data]
+      postNewItem.value = ref(
+        {
+          name:"",
+          category:"",
+          aisle:""
+        }
+      )
+    })
+  }
+
+  const handleDelete = (id) =>{
+    axios.delete('https://grocerylists-backend.herokuapp.com/api/items/' + id)
+    .then((response)=>{
+      foods.value = foods.value.filter(food => food.id !== id)
+    })
+  }
 
 </script>
 
+
 <template>
 <h1>Master List</h1>
-  <div v-for="food in foods">
+  <div class="box" v-for="food in foods">
     <GET :food="food"/>
+    <DELETE :handleDelete="handleDelete" :food="food"/>
   </div>
   <POST :handleCreate = "handleCreate" :postNewItem = "postNewItem"/>
 </template>
 
 <style>
-body{
-  color: red;
+.box{
+  border:2px solid gold;
 }
 </style>
---
+++ src/components/delete.vue
@@ -0,0 +1,14 @@
+<script>
+export default{
+  props:["food", "handleDelete"],
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="handleDelete(food.id)">DELETE</button>
+</template>
